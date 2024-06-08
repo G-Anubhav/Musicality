4dbd8fce5cf37358ab6fd8fd833222a2bb8f891c
G-Anubhav <anubhavgoyal868@gmail.com>
2024-06-09 00:05:43 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNrL8ZY2e8YiVeC3xXD7bxtdjv2_elvr8",
+  authDomain: "jiosaavn-77aaf.firebaseapp.com",
+  projectId: "jiosaavn-77aaf",
+  storageBucket: "jiosaavn-77aaf.appspot.com",
+  messagingSenderId: "112983356655",
+  appId: "1:112983356655:web:329e1bbd8b594ba10bd71a",
+};
+
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
+export { auth, firebase };
+// export default auth;
